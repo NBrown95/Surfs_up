a83950d0dd008f9c3a87cac7a5e5ad9f00b0cd35
NBrown95 <natebrown918@gmail.com>
2021-06-13 18:02:25 -0500

    Created welcome route.

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
-from flask import Flask
+# import dependencies
+from flask import Flask, jsonify
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+# Create engine
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# Reflect database
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# Save references to tables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create session link
+session = Session(engine)
+
 # Create new Flask app instance
 app = Flask(__name__)
 # Create route
 @app.route('/')
-def hello_world():
-    return 'Hello world'
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
